2986651ef7f6c4ab5d066bccb72b5c9d8d27132e
1ris_W <ediath462@gmail.com>
2025-09-19 03:15:06 +0800

    feat: Add demo app

---
+++ Demo/SkinRenderKitDemo/SkinRenderKitDemoApp.swift
@@ -0,0 +1,27 @@
+//
+//  SkinRenderKitDemoApp.swift
+//  SkinRenderKitDemo
+//
+
+import SkinRenderKit
+import SwiftUI
+
+@main
+struct SkinRender_SwiftUIApp: App {
+  var body: some Scene {
+    WindowGroup {
+        SkinRenderView(playerModel: .alex, rotationDuration: 12)
+        .frame(width: 700, height: 500)
+    }
+  }
+}
+
+#Preview("without picker") {
+  SkinRenderView(playerModel: .alex, rotationDuration: 12)
+    .frame(width: 700, height: 500)
+}
+
+#Preview("with picker") {
+  SkinRenderDebug(rotationDuration: 12)
+    .frame(width: 700, height: 700)
+}
